d1ccef84fc3ee8063ee89b4296c1226930c8ab70
Rishusingh7388 <rishutoshsingh199@gmail.com>
2023-06-04 15:05:58 +0530

    loginPage JS

--- loginPage.js
+++ loginPage.js
@@ -1,18 +1,18 @@
 document.querySelector("#btn").addEventListener("click", myAcc)
 
-    var accArr = JSON.parse(localStorage.getItem("number"))||[]
+    var accArr = JSON.parse(localStorage.getItem("numbers"))||[]
 
     function myAcc() {
         event.preventDefault()
         
         var accObj = {
-            num: document.querySelector("#phone").value 
+            num: document.querySelector("#phone").value
         } 
 
         if(accObj.num.length === 10){
             accArr.push(accObj)
         
-            localStorage.setItem("number", JSON.stringify(accArr))
+            localStorage.setItem("numbers", JSON.stringify(accArr))
 
             window.location.href = "loginOtp.html"
         }else{
